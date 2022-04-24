ca2c8a679db9adcf4538ffa04bd7da2601b08b94
Chirag Wadhwa <chiragwadhwa.55555@gmail.com>
2022-04-24 11:24:56 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import flask
+import io
+import string
+import time
+import os
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
